4386de155c2bccecb9c5c00f1d0ea91ab385f0ea
ame <shachi10969@gmail.com>
2023-06-08 19:25:28 +0900

    Initial Commit

---
+++ QiitaArticleList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  QiitaArticleList
+//
+//  Created by t&a on 2023/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QiitaArticleList/QiitaArticleListApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaArticleListApp.swift
+//  QiitaArticleList
+//
+//  Created by t&a on 2023/06/08.
+//
+
+import SwiftUI
+
+@main
+struct QiitaArticleListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
